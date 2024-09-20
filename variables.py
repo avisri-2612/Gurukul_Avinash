42b6a7e18c0efa15870aad1b62928dd3a9d32a3c
avisri-2612 <avinash.srivastava@nokia.com>
2024-09-20 22:56:51 +0530

    assignment

---
+++ variables.py
@@ -0,0 +1,29 @@
+# ##Define a variable as Integer(1) ,Float(1.0) and String(‘1’) and print and return the value and type of variable
+# # x=5
+# # y=2.0
+# # z='avi'
+# # print('x:',x,type(x),'y:',y,type(y),'z:',type(z))
+# ##Redeclare the same variable as another number.(2,2.0 and ‘2’) and share your observation on result.
+# # x=5
+# # y=2.0
+# # z='avi'
+# # print('x:',x,type(x),'y:',y,type(y),'z:',type(z))
+# ##3. Assigning a single value to several variables simultaneously with “=” operators.(a=b=c=10) and print the values of a,b and c..
+# a=b=c=10
+# print('a=',a,type(a),'b=',b,type(b),'c=',c,type(c))
+# ###Assign two variables a and b as integer and print the sum of a+b.
+# x=int(5)
+# y=int(4)
+# print('sum=',x+y)
+# ###Write a Python program to create a list with five different fruits. eg:fruits = ["apple", "banana", "cherry", "date", "elderberry"]?
+# fruits=["apple", "banana", "cherry", "date", "elderberry"]
+# print(fruits)
+# ##6. How do you access the second and fourth elements from the list.
+#
+# print(fruits[3],fruits[1])
+##7.Modify the third element in the list numbers = [10, 20, 30, 40, 50] to 35.
+#
+# number=[10,20,30,40,50]
+# print(number)
+ number[1]=35
+ print(number)
