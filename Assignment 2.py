65fef34a9dcc6c9e7689938b83fcb523361cfbbb
avisri-2612 <avinash.srivastava@nokia.com>
2024-09-22 23:01:21 +0530

    assignment

---
+++ Assignment 2.py
@@ -0,0 +1,51 @@
+#Question 1: Write a Python program that takes two numbers as input from the user and performs the following operations
+# x= int(input("enter the first number"))
+# y=int(input("enter second number"))
+# ##z= x+y
+# ###print(z)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y)
+##
+##Question 2: Write a Python program that checks whether a given number is even or odd using the modulus operator.
+
+# if x%2 == 0:
+#     print("even number")
+# else:
+#     print("odd")
+#     ##Write a Python program that takes a number as input from the user and checks if it is positive, negative, or zero.
+# if x>0:
+#     print(">0")
+# elif x<0:
+#     print("<0")
+# else:
+#     print("0")
+# if x>=90:
+#     print("A")
+# elif x>=80:
+#     print("B")
+# elif x>=70:
+#     print("c")
+# else:
+#     print("d")
+##Question 5: Write a Python program to create a text file called sample.txt and write the sentence "Hello, this is a sample file." to the file. Then, read and display the content from the file.
+# Create and write to the file
+# with open('sample.txt', 'w') as file:
+#     file.write("Hello, this is a sample file.")
+#
+# # Read and display the content of the file
+# with open('sample.txt', 'r') as file:
+#     content = file.read()
+#     print(content)
+##Question 6: Write a Python program that reads a text file called data.txt and counts the number of words in the file.
+#
+   ## print(content)
+   ##Question 7: Write a Python program to print the numbers from 1 to 10 using a for loop.
+x=int(input("Enter the number:"))
+for i in range(1,11):
+       print(f"{x}*{i}={x*i}")
+#Question 8: Write a Python program to display the multiplication table of a number entered by the user using a for loop.
+
+
+
